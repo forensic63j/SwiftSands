9624832f2a751591c499bed51e3f048ae0eba2a0
bxs9775 <bxs9775@g.rit.edu>
2015-02-22 10:42:17 -0500

    Added classes

---
+++ DataStructures/Party.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwiftSands
+{
+	class Party
+	{
+		#region feilds
+		private List<Player> partyList;
+		#endregion
+
+		#region properties
+		/// <summary>
+		/// Gets the party list.
+		/// </summary>
+		public List<Player> PartyList
+		{
+			get { return partyList}
+		}
+
+		/// <summary>
+		/// Gets partymember at index.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public Player this[int index]
+		{
+			get
+			{
+				if(index > 0 && index < partyList.Count)
+				{
+					return partyList[index];
+				} else
+				{
+					return null;
+				}
+			}
+		}
+		#endregion
+
+		#region methods
+		public Party()
+		{
+			partyList = new List<Player>();
+		}
+		#endregion
+	}
+}
