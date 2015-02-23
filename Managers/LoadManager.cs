09f13c095d7211891397798d4d38ba123d9eec8c
unknown <bxs9775@NM2550-03.main.ad.rit.edu>
2015-02-23 10:52:19 -0500

    Small edits to Save and Load Managers.

--- Managers/LoadManager.cs
+++ Managers/LoadManager.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace SwiftSands.Managers
 {
     class LoadManager
     {
+        #region fields
+        DirectoryInfo saveInfo;
+        #endregion
+
+        #region method
+        public LoadManager()
+        {
+            saveInfo = Directory.CreateDirectory("Data//SaveFiles"); 
+        }
+
+        /// <summary>
+        /// Loads content.
+        /// </summary>
+        public void LoadContent()
+        {
+
+        }
+
+        /// <summary>
+        /// Loads a savefile.
+        /// </summary>
+        /// <param name="filename">The name of the file to load.</param>
+        public void LoadSavefile(String filename)
+        {
+            try
+            {
+                using (BinaryReader input = new BinaryReader(filename))
+                {
+
+                }
+            }
+        }
+        #endregion
     }
 }
