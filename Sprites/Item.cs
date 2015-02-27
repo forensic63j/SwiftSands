2e5cae4f929a9d1f6c23dfe3063afb126885756c
forensic63j <forensic63j@gmail.com>
2015-02-27 11:26:33 -0500

    Update Item.cs

--- Sprites/Item.cs
+++ Sprites/Item.cs
@@ -5,7 +5,79 @@ using System.Text;
 
 namespace SwiftSands
 {
+	enum ItemType
+	{
+		Weapon,
+		HealingSpell,
+		AttackSpell,
+		Evidence
+	}
 	class Item:Sprite
 	{
+		private ItemType type;
+		private int healing;
+		private int damage;
+		private String name;
+		private String description;
+		public Item(ItemType i, int h, int d, String n, String de)
+		{
+			itemType = i;
+			healing = h;
+			damage = d;
+			name = n;
+			description = de;
+		}
+		
+		public ItemType Type
+		{
+			get
+			{
+				return type;
+			}
+			set
+			{
+				type = value;
+			}
+		}
+		public int Healing
+		{
+			get
+			{
+				return healing;
+			}
+			set
+			{
+				healing = value;
+			}
+		}
+		public int Damage
+		{
+			get
+			{
+				return damage;
+			}
+			set
+			{
+				damage = value;
+			}
+		}
+		public String Name
+		{
+			get
+			{
+				return name;
+			}
+		}
+		public String Description
+		{
+			get
+			{
+				return description;
+			}
+			set
+			{
+				description = value;
+			}
+		}
 	}
 }
