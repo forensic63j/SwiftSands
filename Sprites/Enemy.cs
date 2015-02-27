0471396faf530d77fc08fbc691daa8ae0f4ef158
forensic63j <forensic63j@gmail.com>
2015-02-27 11:39:22 -0500

    Update Enemy.cs

--- Sprites/Enemy.cs
+++ Sprites/Enemy.cs
@@ -7,10 +7,18 @@ namespace SwiftSands
 {
 	class Enemy:Character
 	{
-		private int expAwarded;
+		private int expAwarded; //How much exp this enemy gives when it's defeated
 		public Enemy(int h, int m, int sp, int st, int a, int l, bool c, String n, int e):base(sp, h, m, st, a, l, c, n)
 		{
 			expAwarded = e;
+		}
+		
+		public void Defeat(Party party)
+		{
+			for(int i = 0; i < party.Count; i++)
+			{
+				party[i].Exp += expAwarded;
+			}
 		}
 	}
 }
