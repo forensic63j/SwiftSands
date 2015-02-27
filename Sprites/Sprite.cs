c205ffccaad9ebc0e287f102a8e819963e79b71f
bxs9775 <bxs9775@g.rit.edu>
2015-02-27 12:44:51 -0500

    Changed sprite
    Switched position back from vector to rectangle

--- Sprites/Sprite.cs
+++ Sprites/Sprite.cs
@@ -11,7 +11,7 @@ namespace SwiftSands
 	{
 		#region fields
 		private Texture2D texture;
-		private Vector2 position;
+		private Rectangle position;
 		#endregion
 
 		#region properties
@@ -26,7 +26,7 @@ namespace SwiftSands
 		/// <summary>
 		/// Gets position.
 		/// </summary>
-		public Vector2 Position
+		public Rectangle Position
 		{
 			get { return position; }
 		}
