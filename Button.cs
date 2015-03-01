fa20cd25020ee05202f015f1ba5e3512ab3872e1
bxs9775 <bxs9775@g.rit.edu>
2015-03-01 17:21:27 -0500

    Button constructor and description.
    Instantiates fields in constructor and includes a descriptive comment on
    the constructor.

--- Button.cs
+++ Button.cs
@@ -13,6 +13,7 @@ namespace SwiftSands
 		private String name;
 		private SpriteFont font;
 		public delegate void Clicked();
+		public Clicked onClick;
 		#endregion
 
 		#region properties
@@ -26,10 +27,22 @@ namespace SwiftSands
 		#endregion
 
 		#region methods
+		/// <summary>
+		/// Creates a new button
+		/// </summary>
+		/// <param name="name">Name of the button, this is displayed as text.</param>
+		/// <param name="font">Font used for the button.</param>
+		/// <param name="sprite">Uses a button sprite from content.</param>
+		/// <param name="position">A rectangle describing the position and size of the button.</param>
+		/// <param name="active">Tells whether a button is active and should appear on screen.</param>
+		/// <param name="onScreen">Tells whether the button is within the window range.</param>
+		/// <param name="clickDelegate"></param>
 		public Button(String name,SpriteFont font,Texture2D sprite,Rectangle position,bool active,bool onScreen,Clicked clickDelegate)
 			: base(sprite,position,active,onScreen)
 		{
-
+			this.name = name;
+			this.font = font;
+			onClick = clickDelegate;
 		}
 		#endregion
 	}
