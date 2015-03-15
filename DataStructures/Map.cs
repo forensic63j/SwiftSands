c5fe7e3047f6355bf6de7ee58b7867bcec3b86b3
bxs9775 <bxs9775@g.rit.edu>
2015-03-15 12:47:00 -0400

    Cited work.
    I added the names of each contribiuter to their respective parts of the
    project. Please check to make sure, your work has been properly
    attributed to you.

--- DataStructures/Map.cs
+++ DataStructures/Map.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Clayton Scavone
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- States/PauseMenu.cs
+++ States/PauseMenu.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Clayton Scavone and Brian Sandon
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
